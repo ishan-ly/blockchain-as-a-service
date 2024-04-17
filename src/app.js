ae8af322a0b723287647605ec2d8de27f4e27846
Ishan Agrawal <ishan.agrawal@loyyal.com>
2024-04-17 15:07:41 +0530

    initial project setup

---
+++ src/app.js
@@ -0,0 +1,52 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const dotenv = require('dotenv');
+const cors = require("cors");
+const http = __importStar(require("http"));
+const controllers_1 = __importDefault(require("./controllers"));
+dotenv.config();
+const app = (0, express_1.default)();
+app.options('*', cors());
+app.use(cors());
+app.use((0, body_parser_1.default)({ limit: '50mb' }));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({
+    extended: false,
+}));
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World!' });
+});
+(0, controllers_1.default)(app);
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+});
+server.timeout = 240000;
